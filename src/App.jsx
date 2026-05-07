f2d64071c729b2b0bf7a96f888aac5b9e765feae
laperlyn <laperlyn@gmail.com>
2026-05-08 03:02:59 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,25 +8,32 @@ import Wwyd from './pages/Wwyd';
 import Facts from './pages/Facts';
 import Stories from './pages/Stories';
 import Letter from './pages/Letter';
+import DressPery from './pages/DressPery'; // WE IMPORTED THE NEW PAGE HERE
+
+import { StardustProvider } from './context/StardustContext';
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Navbar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/quiz" element={<Quiz />} />
-            <Route path="/wwyd" element={<Wwyd />} />
-            <Route path="/facts" element={<Facts />} />
-            <Route path="/stories" element={<Stories />} />
-            <Route path="/letter" element={<Letter />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <StardustProvider>
+      <Router>
+        <div className="app-container">
+          <Navbar />
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/quiz" element={<Quiz />} />
+              <Route path="/wwyd" element={<Wwyd />} />
+              <Route path="/facts" element={<Facts />} />
+              <Route path="/stories" element={<Stories />} />
+              <Route path="/letter" element={<Letter />} />
+              {/* WE ADDED THE NEW ROUTE RIGHT HERE */}
+              <Route path="/dress-pery" element={<DressPery />} /> 
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </StardustProvider>
   );
 }
 
