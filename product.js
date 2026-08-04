349e86eccc96877b66181c2bfdda46b493cb29fc
laperlyn <laperlyn@gmail.com>
2026-08-04 21:04:09 +0530

    Update product.js

--- product.js
+++ product.js
@@ -1,1 +1,16 @@
-
+const products = [
+  {
+    id: 1,
+    name: "Amaya Banarasi Silk",
+    price: 18900,
+    category: "Silk",
+    image: "assets/images/amaya.jpg"
+  },
+  {
+    id: 2,
+    name: "Meher Lavender Organza",
+    price: 14200,
+    category: "Organza",
+    image: "assets/images/meher.jpg"
+  }
+];
